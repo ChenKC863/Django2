6a272c7f74cbe5ba239f0f13dfaa3eee7b7a1ad3
ChenKC863 <Steven710382@gmail.com>
2024-07-05 16:54:40 +0800

    modify some words

---
+++ blog_spot/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from blog_spot.models import Post, Guestbook
+
+# Register your models here.
+admin.site.register(Post)
+admin.site.register(Guestbook)
---
+++ blog_spot/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.CharField(max_length= 200)
+    content = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True) 
+    
+    class Meta:
+        ordering = ['-pub_date']
+        
+    def __str__(self):
+       return self.title
+   
+class Guestbook(models.Model):
+    name = models.CharField(max_length=20)
+    email = models.CharField(max_length=200)
+    phone = models.CharField(max_length=20)
+    message = models.TextField()
+    req_time = models.DateTimeField(auto_now_add=True)
+   
+    class Meta:
+        ordering = ['-req_time']
+        
+    def __str__(self):
+       return self.name
